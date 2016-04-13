c47380e0d0735ca59a5367772a93fcaf70c4dabe
tonysathre <tony@ghostbit.org>
2016-04-13 15:16:56 -0500

    Added files via upload

---
+++ Set-CMApplicationDeploymentAgent.ps1
@@ -0,0 +1,29 @@
+﻿#Requires -RunAsAdministrator
+param (
+    [Parameter(Mandatory=$true)]
+        [switch]$Enabled
+)
+
+try {
+    $Namespace = 'root\ccm\policy\machine\requestedconfig'
+    $Class = 'CCM_ApplicationManagementClientConfig'
+    $ApplicationManagementAgent = (Get-WmiObject -Namespace $Namespace -Class $Class)
+
+    if ($ApplicationManagementAgent -is [array]) {
+        $ApplicationManagementAgent = $ApplicationManagementAgent[0]
+    }
+
+    $Properties = @{
+        ComponentName   = 'Disable Application Deployment Agent'
+        Enabled         = $Enabled.ToBool()
+        PolicySource    = 'Local'
+        PolicyVersion   = '1.0'
+        PolicyID        = 'DisableAppDeployment'
+        SiteSettingsKey = '1'
+    }
+
+    Set-WmiInstance -InputObject $ApplicationManagementAgent -Arguments $Properties -PutType UpdateOrCreate
+}
+catch {
+    throw $Error[0]
+}
---
+++ Set-CMSoftwareDistibutionAgent.ps1
@@ -0,0 +1,29 @@
+﻿#Requires -RunAsAdministrator
+param (
+    [Parameter(Mandatory=$true)]
+        [switch]$Enabled
+)
+
+try {
+    $Namespace = 'root\ccm\policy\machine\requestedconfig'
+    $Class = 'CCM_SoftwareDistributionClientConfig'
+    $SoftwareDistributionAgent = (Get-WmiObject -Namespace $Namespace -Class $Class)
+    
+    if ($SoftwareDistributionAgent -is [array]) {
+        $SoftwareDistributionAgent = $SoftwareDistributionAgent[0]
+    }
+
+    $Properties = @{
+        ComponentName   = 'Disable Software Distribution Agent'
+        Enabled         = $Enabled.ToBool()
+        PolicySource    = 'Local'
+        PolicyVersion   = '1.0'
+        PolicyID        = 'DisableSoftwareDist'
+        SiteSettingsKey = '1'
+    }
+    
+    Set-WmiInstance -InputObject $SoftwareDistributionAgent -Arguments $Properties -PutType UpdateOrCreate
+}
+catch {
+    throw $Error[0]
+}
