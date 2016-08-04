e534d858bae14b0a901f30f551a72178ecb63b82
tonysathre <tony@ghostbit.org>
2016-08-04 10:32:13 -0500

    Add files via upload

---
+++ Get-PrinterDriversInUse.ps1
@@ -0,0 +1,7 @@
+function Get-PrinterDriversInUse {
+    param (
+        [Parameter(Mandatory=$false)]
+        [string]$ComputerName = $env:COMPUTERNAME   
+    )
+    Get-Printer -ComputerName $ComputerName | select @{Name='Name';Expression={$_.DriverName}} -Unique | Get-PrinterDriver -ComputerName $ComputerName
+}
